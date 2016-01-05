37a9482f342da04d905bb8ef3dd3bf95f99b1b43
Brad <bprymicz@pscu.com>
2016-01-04 23:51:57 -0500

    adding body parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -14,7 +18,7 @@ app.get('/', function(request, response) {
 });
 
 app.post('/webhook', function(request, response) {
-  console.log('webhook: ', request)
+  console.log('webhook: ', JSON.stringify(request.body));
   response.send('thanks')
 });
 
