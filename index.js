969a8143daaa9126c5f7a8878508e75579f6ff51
Brad <bprymicz@pscu.com>
2016-01-04 23:39:21 -0500

    adding webhook endpoint

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.post('/webhook', function(request, response) {
+  console.log('webhook: ', request)
+  response.send('thanks')
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
