ee47687c4b47618984eef0dd1b381fb03ddc7fba
Brad <bprymicz@pscu.com>
2016-03-29 23:24:47 -0400

    de-duping

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var _ = require('lodash');
 var server = require('http').createServer(app);
 var requestLib = require('request');
+var moment = require('moment');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
@@ -31,115 +32,103 @@ var CLOVER_HOST = 'api.clover.com';
 var CLOVER_URL = 'api.clover.com';
 var CLOVER_AUTH = 'Bearer 2e93a7c8-8488-afba-b485-f7724759507a'; //'Bearer 905312bf-99ae-a494-4ff4-f5390cfe63d2';
 
+var currentTaps = {
+  beerData: [],
+  last_updated: null
+};
+
 
 //https://api.clover.com:443/v3/merchants/NATHK6XKV2BF4/items?access_token=
 app.get('/taps', function(request, response) {
   var options = {
     port: 443,
-    host: CLOVER_URL,
-    path: '/v3/merchants/NATHK6XKV2BF4/items?expand=tags%2Ccategories&limit=1000', // dev acct
-    method: 'GET',
-    headers: {
-      'Authorization': CLOVER_AUTH,
-      'Accept-Encoding': 'gzip',
-      'Cache-Control': {'max-age': 0},
-      'Host': CLOVER_HOST,
-      'Content-type': 'application/json'
-    }
+    uri: 'https://' + CLOVER_URL + '/v3/merchants/NATHK6XKV2BF4/items?expand=tags%2Ccategories&limit=1000&access_token=2e93a7c8-8488-afba-b485-f7724759507a', // dev acct
+    method: 'GET'
   };
-  var req = https.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    var chunks = [];
-    res.on('data', function(chunk) {
-      chunks.push(chunk);
-    });
-    res.on('end', function() {
-      //console.log(chunked);
-      console.log('No more data in response.')
-    });
-    res.on('end', function() {
-      var buffer = Buffer.concat(chunks);
-      var encoding = res.headers['content-encoding'];
-      if (encoding == 'gzip') {
-        zlib.gunzip(buffer, function(err, decoded) {
-          if (decoded && decoded.toString()) {
-            var resp = JSON.parse(decoded.toString());
-            filterByCategory(resp,'Draft Beer', function (data){
-              response.send(data);
-            });
-          }
-        });
-      } else if (encoding == 'deflate') {
-        zlib.inflate(buffer, function(err, decoded) {
-          if (decoded && decoded.toString()) {
-            var resp = JSON.parse(decoded.toString());
-            filterByCategory(resp,'Draft Beer', function (data){
-              response.send(data);
-            });
-          }
-        })
-      } else {
-        response.send(buffer.toString());
-      }
-    });
+
+  requestLib.get(options, function(err, httpResponse, body) {
+    var data = JSON.parse(body);
+    response.write(JSON.stringify(data, null, 2));
+
+  }).on('response', function(response) {
+    console.log(response.statusCode) // 200
+    console.log(response.headers['content-type']) // 'image/png'
   });
 
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-    response.send({error: e});
-  });
-
-  req.end();
 });
 
 
 app.get('/widget', function(req,resp) {
 
-  var options = {
-    port: 443,
-    uri: 'https://' + CLOVER_URL + '/v3/merchants/NATHK6XKV2BF4/items?expand=tags%2Ccategories&limit=1000&access_token=2e93a7c8-8488-afba-b485-f7724759507a', // dev acct
-    method: 'GET'
-  };
   var params = req.query;
 
-  requestLib.get(options, function(err,httpResponse,body){
-    var data = JSON.parse(body);
-    var category = params.category ? params.category : 'all';
-    resp.write('<div><ul>');
-    if (category === 'all') {
-      data.elements.forEach(function (matches){
-        respondBeer(matches, function (display) {
-          resp.write(display);
-        });
-      });
-    } else {
-      filterByCategory(data, category, function (matches){
-        respondBeer(matches, function (display) {
-          resp.write(display);
-        });
-      });
-    }
-    resp.write('</ul></div>');
+  if (currentTaps.last_updated) {
+    console.log('using stored data');
+    var data = currentTaps.beerData;
+    data.category = params.category ? params.category : 'all';
+    createHTML(data, function(html) {
+      console.log(html);
+      resp.write(html);
+    })
+  } else {
+    console.log('using new data');
+    var options = {
+      port: 443,
+      uri: 'https://' + CLOVER_URL + '/v3/merchants/NATHK6XKV2BF4/items?expand=tags%2Ccategories&limit=1000&access_token=2e93a7c8-8488-afba-b485-f7724759507a', // dev acct
+      method: 'GET'
+    };
 
-  }).on('response', function(response) {
+    requestLib.get(options, function(err, httpResponse, body) {
+      var data = JSON.parse(body);
+      currentTaps.last_updated = moment();
+      currentTaps.beerData = data;
+      data.category = params.category ? params.category : 'all';
+      createHTML(data, function(html) {
+        console.log(html);
+        resp.write(html);
+        resp.end();
+      })
+
+    }).on('response', function(response) {
       console.log(response.statusCode) // 200
       console.log(response.headers['content-type']) // 'image/png'
     });
+  }
+
 
 });
+
+function createHTML(data, cb) {
+  getCategoryHTML(data, function(htmlResp) {
+    var html = '<div style="display: flex; flex-direction: column; flex-wrap: wrap">';
+    html += htmlResp;
+    html +='</div>';
+    cb(html);
+  });
+}
+
+function getCategoryHTML(data, cb) {
+  filterByCategory(data, data.category, function (matches){
+    respondBeer(matches, function (display) {
+      cb(display);
+    });
+  });
+}
 
 function respondBeer(beers, cb) {
   var data_beers = _.sortBy(beers, function(aBeer) {
     return aBeer.name;
   });
+  var beerText = '';
   data_beers.forEach(function (beer){
-    var beerText = '';
     if (beer.tapNumber) {
-      beerText = '<li>' + beer.tapNumber + ' ' + beer.beerName + '</li>'
+      beerText += '<div style="display: flex; flex-direction: row">' +
+        '<div style="width: 40px">' + beer.tapNumber + '</div>' +
+        '<div>' + beer.beerName + '</div>' +
+        '</div>';
     }
-    cb(beerText);
-  })
+  });
+  cb(beerText);
 }
 
 server.listen(app.get('port'), function() {
@@ -147,10 +136,10 @@ server.listen(app.get('port'), function() {
 });
 
 function filterByCategory(data, category, cb) {
-  var matches = [];
+  var tapSpots = {};
   data.elements.forEach(function(item) {
     if (item.categories) {
-      var index = _.findIndex(item.categories.elements, function(o) { return o.name == category });
+      var index = _.findIndex(item.categories.elements, function(o) { return category === 'all' || o.name == category });
       var tapNumber = item.name.substr(0, 2);
       item.beerName = item.name.substr(3);
 
@@ -158,9 +147,15 @@ function filterByCategory(data, category, cb) {
         item.tapNumber = Number(tapNumber);
       }
       if (index >= 0 && item.stockCount > 0) {
-        matches.push(item);
+        if (!tapSpots['tap' + tapNumber] || tapSpots['tap' + tapNumber].modifiedTime > item.modifiedTime) {
+          tapSpots['tap' + tapNumber] = item;
+        }
       }
     }
+  });
+  var matches = [];
+  Object.keys(tapSpots).forEach(function(tap) {
+    matches.push(tapSpots[tap]);
   });
   cb(matches);
 }
